a8760df3d02262c5ff64f4978e1dbfb12d1fe8b8
Matthew Pullen <matthew.pullen@gmail.com>
2024-03-17 17:23:49 -0500

    Added filters

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: TabsScreen(),
+      home: const TabsScreen(),
     );
   }
 }
